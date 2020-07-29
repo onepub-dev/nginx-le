ee36a02d36ce6fc7b63513da29fdcca6e4f5de6c
Brett Sutton <bsutton@noojee.com.au>
2020-07-29 23:04:24 +1000

    New certificate class allows us to parse the certbot certificates.

---
+++ shared/lib/src/certbot/certificate.dart
@@ -0,0 +1,112 @@
+import 'package:dshell/dshell.dart';
+import 'package:instant/instant.dart';
+import 'package:nginx_le_shared/src/certbot/certbot.dart';
+
+class Certificate {
+  String fqdn;
+
+  String domains;
+
+  DateTime expiryDate;
+
+  /// If [staging] is true then this is a production certificate
+  /// If [staging] is false then this is a staging/test certificate.
+  bool staging;
+
+  String certificatePath;
+
+  String privateKeyPath;
+
+  void parseName(String line) {
+    var parts = line.split(':');
+    fqdn = parts[1].trim();
+  }
+
+  void parseDomains(String line) {
+    var parts = line.split(':');
+    domains = parts[1].trim();
+  }
+
+  void parseExpiryDate(String line) {
+    var parts = line.split('Date:');
+    var expiryDateString = parts[1].trim();
+
+    var datePart = expiryDateString.substring(0, 25);
+    expiryDate = DateTime.parse(datePart); // 'yyyy-MM-dd hh:mm:ss+');
+    staging = line.contains('TEST_CERT');
+  }
+
+  void parseCertificatePath(String line) {
+    var parts = line.split(':');
+    certificatePath = parts[1].trim();
+  }
+
+  void parsePrivateKeyPath(String line) {
+    var parts = line.split(':');
+    privateKeyPath = parts[1].trim();
+  }
+
+  static List<Certificate> load() {
+    var cmd = 'certbot certificates '
+        ' --config-dir=${Certbot.letsEncryptConfigPath}';
+
+    var lines = cmd.toList(nothrow: true);
+
+    return parse(lines);
+  }
+
+  /// When certs exist we get
+  ///
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Found the following certs:
+//   Certificate Name: slayer.noojee.org
+//     Domains: slayer.noojee.org
+//     Expiry Date: 2020-10-27 06:10:05+00:00 (INVALID: TEST_CERT)
+//     Certificate Path: /etc/letsencrypt/config/live/slayer.noojee.org/fullchain.pem
+//     Private Key Path: /etc/letsencrypt/config/live/slayer.noojee.org/privkey.pem
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+// when no certificates found.
+//  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// No certs found.
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  static List<Certificate> parse(List<String> lines) {
+    var certificates = <Certificate>[];
+
+    Certificate cert;
+    for (var line in lines) {
+      if (line.trim().startsWith('Certificate Name:')) {
+        cert = Certificate();
+        certificates.add(cert);
+        cert.parseName(line);
+      }
+      if (line.trim().startsWith('Domains:')) {
+        cert.parseDomains(line);
+      }
+      if (line.trim().startsWith('Expiry Date:')) {
+        cert.parseExpiryDate(line);
+      }
+      if (line.trim().startsWith('Certificate Path')) {
+        cert.parseCertificatePath(line);
+      }
+      if (line.trim().startsWith('Private Key Path:')) {
+        cert.parsePrivateKeyPath(line);
+      }
+    }
+    return certificates;
+  }
+
+  bool hasExpired() {
+    return (expiryDate.isBefore(DateTime.now()));
+  }
+
+  @override
+  String toString() {
+    var offset = DateTime.now().timeZoneOffset;
+    var hours = offset.inHours + offset.inMinutes / 60;
+    return '''Name: $fqdn Domains: $domains Expiry: ${dateTimeToOffset(datetime: expiryDate, offset: hours)}
+    Certificate Path: $certificatePath
+    Private Key Path: $privateKeyPath''';
+  }
+}
---
+++ shared/test/src/certbot/certificates_test.dart
@@ -0,0 +1,100 @@
+import 'package:nginx_le_shared/nginx_le_shared.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Certificates', () {
+    test('With Staging Certificate', () {
+      var lines =
+          ''' - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ Found the following certs:'
+   Certificate Name: slayer.noojee.org
+     Domains: slayer.noojee.org
+     Expiry Date: 2020-10-27 06:10:05+00:00 (INVALID: TEST_CERT)
+     Certificate Path: /etc/letsencrypt/config/live/slayer.noojee.org/fullchain.pem
+     Private Key Path: /etc/letsencrypt/config/live/slayer.noojee.org/privkey.pem
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'''
+              .split('\n');
+
+      var certificates = Certificate.parse(lines);
+      expect(certificates.length, equals(1));
+      var certificate = certificates[0];
+      expect(certificate.fqdn, equals('slayer.noojee.org'));
+      expect(certificate.domains, equals('slayer.noojee.org'));
+      expect(
+          certificate.certificatePath,
+          equals(
+              '/etc/letsencrypt/config/live/slayer.noojee.org/fullchain.pem'));
+      expect(certificate.privateKeyPath,
+          equals('/etc/letsencrypt/config/live/slayer.noojee.org/privkey.pem'));
+      expect(certificate.staging, equals(true));
+      expect(certificate.expiryDate,
+          equals(DateTime.parse('2020-10-27 06:10:05+00:00')));
+    });
+
+    test('No Certificates', () {
+      var lines =
+          ''' - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ No certs found.
+ - - - - - - - - - - - - - -'''
+              .split('\n');
+
+      var certificates = Certificate.parse(lines);
+      expect(certificates.length, equals(0));
+    });
+
+    test('Has expired', () {
+      var lines =
+          ''' - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ Found the following certs:'
+   Certificate Name: slayer.noojee.org
+     Domains: slayer.noojee.org
+     Expiry Date: 1920-10-27 06:10:05+00:00 (INVALID: TEST_CERT)
+     Certificate Path: /etc/letsencrypt/config/live/slayer.noojee.org/fullchain.pem
+     Private Key Path: /etc/letsencrypt/config/live/slayer.noojee.org/privkey.pem
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'''
+              .split('\n');
+
+      var certificates = Certificate.parse(lines);
+      expect(certificates.length, equals(1));
+      var certificate = certificates[0];
+      expect(certificate.hasExpired(), equals(true));
+    });
+
+    test('Has Not expired', () {
+      var lines =
+          ''' - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ Found the following certs:'
+   Certificate Name: slayer.noojee.org
+     Domains: slayer.noojee.org
+     Expiry Date: 2030-10-27 06:10:05+00:00 (INVALID: TEST_CERT)
+     Certificate Path: /etc/letsencrypt/config/live/slayer.noojee.org/fullchain.pem
+     Private Key Path: /etc/letsencrypt/config/live/slayer.noojee.org/privkey.pem
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'''
+              .split('\n');
+
+      var certificates = Certificate.parse(lines);
+      expect(certificates.length, equals(1));
+      var certificate = certificates[0];
+      expect(certificate.hasExpired(), equals(false));
+    });
+
+    test('Print Certificate', () {
+      var lines =
+          ''' - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ Found the following certs:'
+   Certificate Name: slayer.noojee.org
+     Domains: slayer.noojee.org
+     Expiry Date: 2030-10-27 06:10:05+00:00 (INVALID: TEST_CERT)
+     Certificate Path: /etc/letsencrypt/config/live/slayer.noojee.org/fullchain.pem
+     Private Key Path: /etc/letsencrypt/config/live/slayer.noojee.org/privkey.pem
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -'''
+              .split('\n');
+
+      var certificates = Certificate.parse(lines);
+      expect(certificates.length, equals(1));
+      var certificate = certificates[0];
+
+      print(certificate);
+    });
+  });
+}
