2ee410f73b875d58387d3bc074b684b9dc97405d
Brett Sutton <bsutton@noojee.com.au>
2020-07-29 13:12:45 +1000

    built a script to automate

--- shared/lib/src/nginx.dart
+++ shared/lib/src/nginx.dart
@@ -5,6 +5,8 @@ class Nginx {
   static const NGINX_ERROR_LOG_ENV = 'NGINX_ERROR_LOG_ENV';
   static const NGINX_LOCATION_INCLUDE_PATH = 'NGINX_LOCATION_INCLUDE_PATH';
 
+  static const DEFAULT_INCLUDE_PATH = '/opt/nginx/includes';
+
   static String get accesslogpath {
     var path = env(NGINX_ACCESS_LOG_ENV);
     path ??= '/var/log/nginx/access.log';
@@ -14,7 +16,7 @@ class Nginx {
   /// The default path where nginx looks for the include files (Locations and Upstream)
   static String get locationIncludePath {
     var path = env(NGINX_LOCATION_INCLUDE_PATH);
-    path ??= '/etc/nginx/includes';
+    path ??= DEFAULT_INCLUDE_PATH;
     return path;
   }
 
