8aa15a56597e277c6b7794638a0dfb6a78b26fe3
Brett Sutton <bsutton@noojee.com.au>
2020-08-20 15:24:39 +1000

    Using wrong environment vars for http auth (there were missing completely).

--- shared/lib/src/certbot/http_auth.dart
+++ shared/lib/src/certbot/http_auth.dart
@@ -17,13 +17,11 @@ void http_auth_acquire({
   var configDir = _createDir(Certbot.letsEncryptConfigPath);
 
   /// These are set via in the Dockerfile
-  var auth_hook = Environment().certbotDNSAuthHookPath;
-  var cleanup_hook = Environment().certbotDNSCleanupHookPath;
+  var auth_hook = Environment().certbotHTTPAuthHookPath;
+  var cleanup_hook = Environment().certbotHTTPCleanupHookPath;
 
-  ArgumentError.checkNotNull(
-      auth_hook, 'Environment variable: CERTBOT_HTTP_AUTH_HOOK_PATH missing');
-  ArgumentError.checkNotNull(cleanup_hook,
-      'Environment variable: CERTBOT_HTTP_CLEANUP_HOOK_PATH missing');
+  ArgumentError.checkNotNull(auth_hook, 'Environment variable: CERTBOT_HTTP_AUTH_HOOK_PATH missing');
+  ArgumentError.checkNotNull(cleanup_hook, 'Environment variable: CERTBOT_HTTP_CLEANUP_HOOK_PATH missing');
 
   var certbot = 'certbot certonly '
       ' --manual '
@@ -42,10 +40,7 @@ void http_auth_acquire({
   if (staging) certbot += ' --staging ';
 
   certbot.start(
-      runInShell: true,
-      nothrow: true,
-      progress:
-          Progress((line) => print(line), stderr: (line) => printerr(line)));
+      runInShell: true, nothrow: true, progress: Progress((line) => print(line), stderr: (line) => printerr(line)));
 }
 
 String _createDir(String dir) {
