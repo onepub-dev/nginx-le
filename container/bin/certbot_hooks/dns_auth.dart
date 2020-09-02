efa90150b2615a51a85974be3e5a4c04be108859
Brett Sutton <bsutton@noojee.com.au>
2020-09-02 14:04:46 +1000

    Fixed a major bug in the namecheap auth. It was calling certbot again rather than the dns hook.

--- container/bin/certbot_hooks/dns_auth.dart
+++ container/bin/certbot_hooks/dns_auth.dart
@@ -13,7 +13,8 @@ import 'package:nginx_le_shared/nginx_le_shared.dart';
 /// Once the TXT record is available we return an let
 ///
 void main() {
-  var authProvider = AuthProviders().getByName(Environment().certbotAuthProvider);
+  var authProvider =
+      AuthProviders().getByName(Environment().certbotAuthProvider);
 
-  authProvider.acquire();
+  authProvider.auth_hook();
 }
