1ba1101a437bd66fa683d4abae9faf3135055e12
Brett Sutton <bsutton@noojee.com.au>
2020-09-03 21:19:22 +1000

    added example.

---
+++ cli/example/example.dart
@@ -0,0 +1,10 @@
+///
+/// This is an executable not a library
+///
+/// Run:
+///
+/// nginx-le config
+///
+/// nginx-le start
+///
+/// You now have an nginx web server with automatic certificate renewal.
