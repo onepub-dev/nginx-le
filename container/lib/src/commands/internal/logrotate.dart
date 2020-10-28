7372057a0c313e1322fef310561d5873f44680f4
Brett Sutton <bsutton@noojee.com.au>
2020-10-28 16:15:12 +1100

    restructured as a class.

--- container/lib/src/commands/internal/logrotate.dart
+++ container/lib/src/commands/internal/logrotate.dart
@@ -1,38 +1,33 @@
+import 'dart:isolate';
+
 import 'package:dcli/dcli.dart';
 import 'package:isolate/isolate_runner.dart';
 
 const CONFIG_FILE = '/etc/nginx/logrotate.conf';
-void logrotate() {
-  print('Running logrotate');
-
-  if (!exists(CONFIG_FILE)) {
-    printerr(
-        red('The logrotate configuration file was not found at: $CONFIG_FILE'));
-  }
-  if (start('logrotate $CONFIG_FILE', nothrow: true, progress: Progress.print())
-          .exitCode !=
-      0) {
-    print(red('Logrotate exited with a non-zero exit code.'));
-  }
-}
 
 ////////////////////////////////////////////
 /// Logrotate thread
 ////////////////////////////////////////////
-void startLogRotateThread({bool debug = false}) {
-  print('Starting the logrotate  scheduler.');
 
-  var iso = waitForEx<IsolateRunner>(IsolateRunner.spawn());
+class LogManager {
+  IsolateRunner isoLogRotate;
+  
+  void start({bool debug = false}) {
+    print('Starting the logrotate  scheduler.');
 
-  try {
-    iso.run(startScheduler, debug ? 'debug' : 'nodebug');
-  } finally {
-    waitForEx(iso.close());
+    isoLogRotate = waitForEx<IsolateRunner>(IsolateRunner.spawn());
+
+    try {
+      isoLogRotate.run(_startScheduler, debug ? 'debug' : 'nodebug');
+      
+    } finally {
+      waitForEx(isoLogRotate.close());
+    }
   }
 }
 
 /// Isolate callback must be a top level function.
-void startScheduler(String debug) {
+void _startScheduler(String debug) {
   Settings().setVerbose(enabled: debug == 'debug');
 
   /// keep the isolate running forever.
@@ -40,6 +35,17 @@ void startScheduler(String debug) {
     /// we do a log rotation every 20 minutes.
     sleep(1200, interval: Interval.seconds);
 
-    logrotate();
+    _logrotate();
+  }
+}
+
+void _logrotate() {
+  print('Running logrotate');
+
+  if (!exists(CONFIG_FILE)) {
+    printerr(red('The logrotate configuration file was not found at: $CONFIG_FILE'));
+  }
+  if (start('logrotate $CONFIG_FILE', nothrow: true, progress: Progress.print()).exitCode != 0) {
+    print(red('Logrotate exited with a non-zero exit code.'));
   }
 }
