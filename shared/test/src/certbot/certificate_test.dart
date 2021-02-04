e19da77b7d2d49a56400a3dffb17ac9bdcf2c0ae
Brett Sutton <bsutton@noojee.com.au>
2021-02-04 17:27:18 +1100

    tried to add test for wasIssuedFor but its hard to get certificates on a local system.

--- shared/test/src/certbot/certificate_test.dart
+++ shared/test/src/certbot/certificate_test.dart
@@ -1,3 +1,4 @@
+import 'package:nginx_le_shared/src/certbot/certbot_paths.dart';
 import 'package:nginx_le_shared/src/certbot/certificate.dart';
 import 'package:test/test.dart';
 
@@ -10,6 +11,21 @@ void main() {
 
     for (var cert in certificates) {
       print(cert.toString());
+    }
+  });
+
+  test('check for certificate with matching details ...', () async {
+    prepareEnvironment();
+
+    print('loading certificates from: ${CertbotPaths.letsEncryptConfigPath}');
+    var certificates = Certificate.load();
+
+    print('Found ${certificates.length} certificates');
+
+    for (var cert in certificates) {
+      print(cert.toString());
+      // if (cert.wasIssuedFor())
+
     }
   });
 }
