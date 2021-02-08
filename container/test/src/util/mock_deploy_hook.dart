57cc36bf1d4e3d514876f4667cc384e440d3b9ff
Brett Sutton <bsutton@noojee.com.au>
2021-02-08 15:42:33 +1100

    removed unused import.

--- container/test/src/util/mock_deploy_hook.dart
+++ container/test/src/util/mock_deploy_hook.dart
@@ -3,7 +3,6 @@
 import 'package:nginx_le_container/src/commands/internal/deploy_hook.dart';
 import 'package:nginx_le_shared/nginx_le_shared.dart';
 
-import 'acquisition_manager_test.dart';
 import 'mock_cerbot_paths.dart';
 
 /// Used by unit tests. This code runs the same logic as deploy_hook
