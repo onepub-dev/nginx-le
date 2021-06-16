4ca4597bb03e7fcf2ddfd62e2a3698e2d88edbdd
Brett Sutton <bsutton@noojee.com.au>
2021-06-16 17:58:45 +1000

    updated comments.

--- container/bin/certbot_hooks/auth_hook.dart
+++ container/bin/certbot_hooks/auth_hook.dart
@@ -6,11 +6,8 @@ import 'package:nginx_le_shared/nginx_le_shared.dart';
 ///
 /// This app is called by Certbot to provide the DNS validation.
 ///
-/// This app creates a DNS TXT record _acme-challenge containing the validation
-/// key.
-/// The app then loops until the TXT record is propergated to local DNS servers.
-///
-/// Once the TXT record is available we return an let
+/// The technique used is dependant on the selected authProvider
+/// which is set via the environment variable: [Environment().authProvider]
 ///
 void main() {
   var authProvider = AuthProviders().getByName(Environment().authProvider!);
