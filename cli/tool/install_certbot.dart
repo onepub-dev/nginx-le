d1d52d214b9de565e634a5108bff52113a8278bd
Brett Sutton <bsutton@noojee.com.au>
2023-06-05 10:40:41 +1000

    tweaked the python venv install to use the lates version rather than .9 which is no longer available.

--- cli/tool/install_certbot.dart
+++ cli/tool/install_certbot.dart
@@ -5,12 +5,10 @@
  * Written by Brett Sutton <bsutton@onepub.dev>, Jan 2022
  */
 
-
-
 import 'package:dcli/dcli.dart';
 
 void main(List<String> args) {
-  'apt install --no-install-recommends -y python3 python3.9-venv'.run;
+  'apt install --no-install-recommends -y python3 python3-venv'.run;
   'python3 -m venv /opt/certbot/'.run;
   '/opt/certbot/bin/pip install --upgrade pip'.run;
   '/opt/certbot/bin/pip install certbot'.run;
